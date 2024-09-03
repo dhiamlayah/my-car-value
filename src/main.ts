172ff136a4ea23eb326aaff8704af5a3f9a29ca1
dhiamlayah <mlayahdhia8@gmail.com>
2024-09-03 23:55:19 +0100

    add first endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,  //that mean any additional propritie in the request bee accepted no problem but this additionly proprity will not be exist in the app for securitie 
+    })
+  )
+
+  console.log('App listen on port 3000')
   await app.listen(3000);
 }
 bootstrap();
