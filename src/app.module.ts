5480e4432724c0494408201a93968877b088e70d
dhiamlayah <mlayahdhia8@gmail.com>
2024-09-03 11:58:38 +0100

    create entites

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entites';
+import { Report } from './reports/reports.entites';
 
 @Module({
-  imports: [UsersModule,ReportsModule,TypeOrmModule.forRoot(
+  imports: [UsersModule, ReportsModule, TypeOrmModule.forRoot(
     {
-      type:"sqlite",
-      database:'db.sqllite',
-      entities:[],
-      synchronize:true , 
+      type: "sqlite",
+      database: 'db.sqllite',
+      entities: [User,Report],
+      synchronize: true,
     }
   )],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
