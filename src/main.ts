0f91fd6c358545efa660a4568d9b2950633bd20b
dhiamlayah <mlayahdhia8@gmail.com>
2024-09-05 09:52:25 +0100

    create user path

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist:true,  //that mean any additional propritie in the request bee accepted no problem but this additionly proprity will not be exist in the app for securitie 
+      whitelist: true,  //that mean any additional propritie in the request bee accepted no problem but this additionly proprity will not be exist in the app for securitie 
     })
   )
 
   console.log('App listen on port 3000')
   await app.listen(3000);
+
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService : UsersService){}
+
     @Post("/signup")
     createUser(@Body() body : CreateUserDto){
-         console.log(body);
+        this.usersService.create(body.email , body.password)
     }
 }
